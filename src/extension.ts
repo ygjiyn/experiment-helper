ce98f8b2c0d9a2031ddf20c754122a78b824a22d
ygjiyn <103875859+ygjiyn@users.noreply.github.com>
2025-06-16 13:54:33 +0900

    remove redundant contents

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,6 @@ export function activate(context: vscode.ExtensionContext) {
 	const workspaceRoot = vscode.workspace.workspaceFolders ? 
     	vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-	const disposable = vscode.commands.registerCommand('experiment-helper.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from Experiment Helper!');
-	});
-
-	context.subscriptions.push(disposable);
-
 	const pythonVenvProvider = new pythonVenvs.PythonVenvProvider(workspaceRoot);
 	context.subscriptions.push(vscode.window.registerTreeDataProvider(
 		'eh.pythonVenvs', pythonVenvProvider
